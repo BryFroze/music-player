379b7df7a90b3bb98df3b9c6f7c28ea3da8e0e9f
FrozenByzz <hlh_dr@163.com>
2017-07-21 11:23:43 +0800

    Revert "初始播放器项目文件"
    This reverts commit 8725c372a868f43a4b6c8d8c55ddd89b12e188b6.

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,25 +1,15 @@
-import React, { Component } from 'react'
-import './style/app.css'
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
-
-import List from '../list/List'
-import Play from '../Play/Play'
+import React, { Component } from 'react';
+import './scss/App.css';
+import '../common/reset.css';
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {}
-    }
-    render() {
-        return (
-            <Router>
-                <div id="app">
-                    <Route exact path="/" component={List} />
-                    <Route path="/play" component={Play} />
-                </div>
-            </Router>
-        )
-    }
+  render() {
+    return (
+      <div className="App">
+		  
+      </div>
+    );
+  }
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './common/js/rem'
-import './common/style/reset.css'
-import './common/style/app.css'
-import App from './components/App/App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './container/App';
+import firstApp from './reducers/first-reducer'
+// import './index.css';
+let store = createStore(firstApp)
 
+store.subscribe(() => console.log(store.getState()))
+// console.log(store.getState())
 ReactDOM.render(
-    <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
-)
+);
