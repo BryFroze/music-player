d3946cd28ec8f97365e3662b9334a1ae549861b5
FrozenByzz <hlh_dr@163.com>
2017-11-10 10:47:38 +0800

    Add code splitting

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react'
 import './style/app.css'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Bundle from './Bundle'
 
 // import List from '../list/List'
-import SongList from 'container/songList/SongList'
-import List from 'container/list/ListContainer'
-import Play from 'container/play/Play'
+// import SongList from 'container/songList/SongList'
+// import List from 'container/list/ListContainer'
+// import Play from 'container/play/Play'
 import RealAudio from 'container/play/RealAudio'
 import Loading from '../loading/Spinner'
+const SongList = (props) => (
+    <Bundle load={() => import('container/songList/SongList')}>
+        {(Comp) => <Comp {...props}/>}
+    </Bundle>
+)
+const List = (props) => (
+    <Bundle load={() => import('container/list/ListContainer')}>
+        {(Comp) => <Comp {...props}/>}
+    </Bundle>
+)
+const Play = (props) => (
+    <Bundle load={() => import('container/play/Play')}>
+        {(Comp) => <Comp {...props}/>}
+    </Bundle>
+)
+
 class App extends Component {
     constructor(props) {
         super(props)
